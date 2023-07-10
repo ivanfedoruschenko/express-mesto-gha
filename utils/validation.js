a4ab33abe6b83742e0df19fd045ef686ba7a9184
Ivan Fedorushchenko <109195438+ivanfedoruschenko@users.noreply.github.com>
2023-07-10 15:52:01 +0300

    bug-fix:fix-validation

--- utils/validation.js
+++ utils/validation.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 /* eslint-disable */
-const regex = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=]{1,256}\.[a-zA-Z0-9\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=]{2,}/gm;
+const regex = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=]{1,256}\.[a-zA-Z0-9\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=]{2,}/;
 
 const validationGetUserById = celebrate({
   params: Joi.object().keys({
